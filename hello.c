502044309ecc2957a728a8fd6aff928a10ce9cc7
Preston Maness <aggroskater@gmail.com>
2013-01-11 00:17:56 -0600

    learning the autotools.

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+ * hello.c
+ *
+ *  Created on: Jan 10, 2013
+ *      Author: preston
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  printf("lolwut.\n");
+
+  return 0;
+}
